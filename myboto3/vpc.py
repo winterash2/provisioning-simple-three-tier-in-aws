f89136d9409d2a348b0d4334f4d1ccb0318e794b
DJLee <winterash2@naver.com>
2022-11-06 10:01:32 +0900

    [ADD] myboto3 모듈에 VPC 클래스 생성, VPC 생성, 삭제 메서드 생성

---
+++ myboto3/vpc.py
@@ -0,0 +1,91 @@
+import sys
+import botocore
+import boto3
+
+class VPC:
+    '''
+    Request
+        CidrBlock='string',
+        AmazonProvidedIpv6CidrBlock=True|False,
+        Ipv6Pool='string',
+        Ipv6CidrBlock='string',
+        Ipv4IpamPoolId='string',
+        Ipv4NetmaskLength=123,
+        Ipv6IpamPoolId='string',
+        Ipv6NetmaskLength=123,
+        DryRun=True|False,
+        InstanceTenancy='default'|'dedicated'|'host',
+        Ipv6CidrBlockNetworkBorderGroup='string',
+        # Tag Specifier 생략
+    Response
+        ec2.Vpc(id='vpc-048ea590fff96db18')
+    
+    Cretae VPC
+        vpc = ec2_resource.create_vpc(CidrBlock='10.0.1.0/24', DryRun=True)
+        #### ec2.Vpc(id='vpc-0b9e49caee88ff459')
+        resource_ids['vpc']=vpc.id
+    '''
+
+    @property
+    def CidrBlock(self):
+        return self._CidrBlock
+    
+    @CidrBlock.setter
+    def CidrBlock(self, value):
+        self._CidrBlock = value
+
+    # 클래스 생성 시 호출되는 초기화 함수, boto3Interfaces를 통해 미리 생성한 ec2_clinet를 전달받음
+    def __init__(self, boto3Interfaces, CidrBlock = '10.0.0.0/16'):
+        self._CidrBlock = CidrBlock # Boto3의 옵션과 혼동되지 않도록 네이밍을 동일하게 함
+        self._ec2_resource = boto3Interfaces['ec2_resource']
+        self._ec2_client = boto3Interfaces['ec2_client']
+        self._id = None
+    
+    # VPC 생성. 옵션값을 함수 인자로 받지 않고, setter를 이용하여 설정해야 함
+    def create_vpc(self):
+        try:
+            response = self._ec2_resource.create_vpc(
+                CidrBlock = self._CidrBlock,
+                # AmazonProvidedIpv6CidrBlock=True|False,
+                # Ipv6Pool='string',
+                # Ipv6CidrBlock='string',
+                # Ipv4IpamPoolId='string',
+                # Ipv4NetmaskLength=123,
+                # Ipv6IpamPoolId='string',
+                # Ipv6NetmaskLength=123,
+                # DryRun=True|False,
+                # InstanceTenancy='default'|'dedicated'|'host',
+                # Ipv6CidrBlockNetworkBorderGroup='string',
+                # TagSpecifications=[
+                #     {
+                #         'ResourceType': 'capacity-reservation'|'client-vpn-endpoint'|'customer-gateway'|'carrier-gateway'|'coip-pool'|'dedicated-host'|'dhcp-options'|'egress-only-internet-gateway'|'elastic-ip'|'elastic-gpu'|'export-image-task'|'export-instance-task'|'fleet'|'fpga-image'|'host-reservation'|'image'|'import-image-task'|'import-snapshot-task'|'instance'|'instance-event-window'|'internet-gateway'|'ipam'|'ipam-pool'|'ipam-scope'|'ipv4pool-ec2'|'ipv6pool-ec2'|'key-pair'|'launch-template'|'local-gateway'|'local-gateway-route-table'|'local-gateway-virtual-interface'|'local-gateway-virtual-interface-group'|'local-gateway-route-table-vpc-association'|'local-gateway-route-table-virtual-interface-group-association'|'natgateway'|'network-acl'|'network-interface'|'network-insights-analysis'|'network-insights-path'|'network-insights-access-scope'|'network-insights-access-scope-analysis'|'placement-group'|'prefix-list'|'replace-root-volume-task'|'reserved-instances'|'route-table'|'security-group'|'security-group-rule'|'snapshot'|'spot-fleet-request'|'spot-instances-request'|'subnet'|'subnet-cidr-reservation'|'traffic-mirror-filter'|'traffic-mirror-session'|'traffic-mirror-target'|'transit-gateway'|'transit-gateway-attachment'|'transit-gateway-connect-peer'|'transit-gateway-multicast-domain'|'transit-gateway-policy-table'|'transit-gateway-route-table'|'transit-gateway-route-table-announcement'|'volume'|'vpc'|'vpc-endpoint'|'vpc-endpoint-connection'|'vpc-endpoint-service'|'vpc-endpoint-service-permission'|'vpc-peering-connection'|'vpn-connection'|'vpn-gateway'|'vpc-flow-log'|'capacity-reservation-fleet'|'traffic-mirror-filter-rule'|'vpc-endpoint-connection-device-type'|'vpn-connection-device-type',
+                #         'Tags': [
+                #             {
+                #                 'Key': 'string',
+                #                 'Value': 'string'
+                #             },
+                #         ]
+                #     },
+                # ]
+            )
+            self._id = response.id
+            return self._id
+        except:
+            print("Unexpected error:", sys.exc_info())
+            return False
+
+    def delete_vpc(self):
+        if self._id == None:
+            print("생성되어 있지 않은 자원입니다.")
+        else:
+            try:
+                response = self._ec2_client.delete_vpc(
+                    VpcId=self._id,
+                )
+                print("VPC 정상적으로 삭제하였습니다.")
+                return True
+            except:
+                print("Unexpected error:", sys.exc_info())
+                return False
+        return True
+    
--- provisioningSimpleThreeTierInAws.py
+++ provisioningSimpleThreeTierInAws.py
@@ -2,6 +2,7 @@ import os
 import botocore
 import boto3
 import yaml
+from myboto3.vpc import VPC
 
 # 실행하면서 생성된 변수들에 대한 정보를 저장할 cache 폴더를 생성
 def createCacheFoler():
@@ -12,7 +13,7 @@ def createCacheFoler():
         # 프로그램을 실행시킨 OS 계정에 문제가 있는 경우
         print("현재 폴더와 하위 폴더에 파일을 읽고 쓸 수 있는 권한을 가진 계정으로 다시 실행하십시오.")
 
-def createBoto3Session(globalConfig):
+def createBoto3Interfaces(globalConfig):
     session = boto3.Session(
         aws_access_key_id=globalConfig['ACCESS_KEY'],
         aws_secret_access_key=globalConfig['SECRET_KEY']
@@ -33,9 +34,22 @@ def createBoto3Session(globalConfig):
             print("AWS 자격증명에 문제가 있습니다. config.yaml 파일의 ACCESS_KEY와 SECRET_KEY를 확인해주시기 바랍니다.")
             return None
         elif err.response['Error']['Code'] == 'DryRunOperation':
-            return session
-    return None # DryRun으로 실행했기 때문에 반드시 except가 발생해야 함. 발생하지 않은 경우 문제가 있는 것으로 판단
+            pass
+        else: # DryRun으로 실행했기 때문에 반드시 except가 발생해야 함. 발생하지 않은 경우 문제가 있는 것으로 판단
+            return None
+    
+    boto3Interfaces = {}
+    boto3Interfaces['ec2_client'] = session.client('ec2', config=my_config)
+    boto3Interfaces['ec2_resource'] = session.resource('ec2', config=my_config)
+    boto3Interfaces['rds_client'] = session.client('rds', config=my_config)
+    boto3Interfaces['elb_client'] = session.client('elbv2', config=my_config)
+    boto3Interfaces['asg_client'] = session.client('autoscaling', config=my_config)
+    # boto3Interfaces['route53domains_client'] = session.client('route53domains', region_name='us-east-1')
+    boto3Interfaces['acm_client'] = session.client('acm', config=my_config)
 
+    return boto3Interfaces 
+
+# config.yaml에 입력되어 있는 설정값들 로드
 def loadConfigFile():
     with open('testconfig.yaml') as f:
         globalConfig = yaml.safe_load(f)
@@ -48,11 +62,31 @@ def main():
     createCacheFoler()
 
     # boto3 session 생성, 문제가 있으면 프로그램 종료
-    session = createBoto3Session(globalConfig)
-    if session == None:
+    boto3Interfaces = createBoto3Interfaces(globalConfig)
+    if boto3Interfaces == None:
         return 1
+
+    threeTierVPC = VPC(boto3Interfaces, "10.0.0.0/16")
+    threeTierVPC_id = threeTierVPC.create_vpc()
     
+
+    print("제거 시작, 아무키나 누를 것")
+    input()
     
+    threeTierVPC.delete_vpc()
+    
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
