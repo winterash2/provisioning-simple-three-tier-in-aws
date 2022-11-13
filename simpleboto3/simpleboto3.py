6c9f8e28678081183869835bb171f2695819fa06
DJLee <winterash2@naver.com>
2022-11-14 02:10:10 +0900

    [ADD] VPC 생성 삭제 추가

--- simpleboto3/simpleboto3.py
+++ simpleboto3/simpleboto3.py
@@ -3,6 +3,7 @@ import os
 import botocore
 import boto3
 import yaml
+from simpleboto3.vpc import *
 from simpleboto3.vpc import VPC
 from simpleboto3.subnet import Subnet
 from simpleboto3.securitygroup import SecurityGroup
@@ -14,28 +15,66 @@ from simpleboto3.securitygroup import SecurityGroup
 # 생성하는 모든 자원에 {'simpleBoto3' : '입력받은 Unique한 이름'} 태그를 붙이고, 
 # 모든 자원의 {'Name' : } 태그를 전부 지정해줌으로써 
 class SimpleBoto3:
-    
+    # Kubernets 처럼 namespace별로 작업. 해당 네임스페이스 내의 모든 자원은 'simpleBoto3':'namespace' 태그를 추가할 것임
+    _namespace = 'default'
+
+    @property
+    def namespace(self):
+        return self._namespace
+
     # myboto3에서와 같이 일일히 boto3Interfaces=boto3Interfaces 와 같이 파라미터를 넘기는 것을 
     # 없애기 위해 SimpleBoto3 라는 클래스로 한 번 더 묶고 여기에서 session이나 client 등을 관리
-    _token = None
     _session = None
     _ec2_client = None
 
+    @property
+    def session(self):
+        return self._session
+    
+    @session.setter
+    def session(self, value):
+        self._session = value
+
+    @property
+    def ec2_client(self):
+        return self._ec2_client
+
+    @property
+    def ec2_resource(self):
+        return self._ec2_resource
+
+
     # 모든 자원을 simpleBoto3 라는 객체에서 전부 가지고 있음으로써 사용을 편리하게 함
     # 자원의 사용은 전부 Name으로 할 수 있도록 함
-    _vpc = None
+    _vpc = None # VPC 클래스 보관
     _subnets = {} # Subnet 클래스들 보관
     _securityGroups = {} # SecurityGroup 클래스들 보관
     
-    def __init__(self):
-        # token을 시용하여 세션을 맺고 검증 테스트를 함
+    def __init__(self, session, namespace='default'):
         # session, 각종 client들 생성하여 내부 변수에 저장
+        self._session = session
+        self._namespace = namespace
+        self._ec2_client = session.client('ec2')
+        self._ec2_resource = session.resource('ec2')
+
+        vpc_id = get_vpc_id_from_namespace(namespace=self.namespace)
+        self._vpc = VPC()
+        if vpc_id == None:# VPC가 생성되어 있지 않은 경우
+            VPC.create_vpc(self)
+        else: # 만약 vpc가 생성되어 있는 경우
+            self._vpc.id = vpc_id
+        
 
         # {'simpleBoto3' : name} 태그를 달고 있는 모든 자원을 AWS에서 조회한다.
         # 조회되는 것이 있으면 내부 변수들에 정보들을 넣는다
         # 이렇게 함으로써 여러 번 실행해도 작업들을 이어서 진행할 수 있도록 함
         print("Class SimpleBoto3")
-    
+
+
+    def describe_vpc(self):
+        print()
+
+
     def test_subent(self):
         test = Subnet()
         
@@ -43,8 +82,10 @@ class SimpleBoto3:
     def set_vpc_options():
         pass
 
+
     def describe_vpc(self, cidrBlock='10.0.0.0/16'):
         pass
+
 
     def get_subnet_list(self):
         pass
---
+++ simpleboto3/tag.py
@@ -0,0 +1,19 @@
+
+
+def ec2_create_tag(simpleBoto3, resource, key, value):
+    try:
+        response = simpleBoto3._ec2_client.create_tags(
+            Resources=[
+                resource.id,
+            ],
+            Tags=[
+                {
+                    'Key': key,
+                    'Value': value
+                },
+            ]
+        )
+    except Exception as err:
+        print("Tag 생성 중 알 수 없는 에러가 발생하였습니다.", str(err))
+        raise
+    return True
